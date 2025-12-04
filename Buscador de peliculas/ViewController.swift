3cee9fedfd6efc35780e13c3565817193b08defd
mananas <u0142730768@gmail.com>
2025-12-04 09:43:01 +0100

    Initial Commit

---
+++ Buscador de peliculas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Buscador de peliculas
+//
+//  Created by Mananas on 4/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
